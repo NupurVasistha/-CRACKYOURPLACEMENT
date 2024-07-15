3b04da6f99e53fdad258d90256af2c11ac6fc38f
Nupur Vasistha <nupurvasistha55@gmail.com>
2024-07-15 12:42:22 +0530

    initial commit

--- Diameter_of_BinaryTree.java
+++ Diameter_of_BinaryTree.java
@@ -1,23 +1,4 @@
 public class Diameter_of_BinaryTree {
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
-
     public static class Solution {
         private int diameter;
 
